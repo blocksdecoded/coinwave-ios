d8e4d3ca3328e7f5671f5a30d85a24cc11ec85cd
Abai Abakirov <abaikirov@gmail.com>
2019-03-19 13:36:06 +0600

    test for CRCoin

--- CryptotrackerTests/Models/CRCoinTests.swift
+++ CryptotrackerTests/Models/CRCoinTests.swift
@@ -18,9 +18,8 @@ class CRCoinTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testInit() {
+        
     }
 
     func testPerformanceExample() {
